5c4b490d601f1c7362745f8cfcdbd131f88194e2
utx <office@utx.com.au>
2014-09-10 12:43:55 +1000

    Update gatest.js

--- gatest.js
+++ gatest.js
@@ -1,4 +1,5 @@
 <script>
+console.log('ga test');
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -6,5 +7,5 @@
 
   ga('create', 'UA-48260134-1', 'abc.net.au');
   ga('send', 'pageview');
-
+console.log('ga test2');
 </script>
