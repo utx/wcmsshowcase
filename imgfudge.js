2d098eadad09dbc8cc1dc58a873596761b46b11d
utx <office@utx.com.au>
2015-04-23 11:29:54 +1000

    Update imgfudge.js

--- imgfudge.js
+++ imgfudge.js
@@ -1,7 +1,6 @@
 window.document.onload = function(){
 $('img').each(function () {
     theimg = $(this).attr('src')
-    alert(theimg);
     theimg = theimg.replace('thumbnail','large');
     $(this).attr('src',theimg)
   
